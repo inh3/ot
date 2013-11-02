78129c2076e5308c33a90cdcf0c7e23099bb41ba
Ivan Hall <ivan.hall@gmail.com>
2013-11-01 23:16:18 -0500

    Adding delete user stored proc.

---
+++ database/delete_user.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+DROP PROCEDURE DeleteUser$$
+
+CREATE PROCEDURE DeleteUser(
+    IN userName VARCHAR(20))
+BEGIN
+    DECLARE foundId INT DEFAULT NULL;
+    DECLARE foundName VARCHAR(20) DEFAULT NULL;
+    
+    SELECT id INTO foundId
+    FROM Users
+    WHERE (user_name = userName);
+    
+    IF foundId IS NOT NULL THEN
+        DELETE FROM Users WHERE id = foundId;
+    END IF;
+    
+    SELECT id FROM Users Where id = foundId;
+END$$
+DELIMITER ;
