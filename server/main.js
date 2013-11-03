67ca6397fd5078157f2d0269b627731d64967ae4
Ivan Hall <ivan.hall@gmail.com>
2013-11-03 13:46:46 -0600

    Updated server to manage a cookie from express examples.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var app = express();
 
+
+// add favicon() before logger() so
+// GET /favicon.ico requests are not
+// logged, because this middleware
+// reponds to /favicon.ico and does not
+// call next()
+app.use(express.favicon());
+
+// custom log format
+if ('test' != process.env.NODE_ENV)
+    app.use(express.logger(':method :url'));
+
+// parses request cookies, populating
+// req.cookies and req.signedCookies
+// when the secret is passed, used 
+// for signing the cookies.
+app.use(express.cookieParser());
+
+// parses json, x-www-form-urlencoded, and multipart/form-data
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/', function(req, res){
-    res.send('Hello World');
+    if (req.cookies.remember) {
+        res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
+    } else {
+        res.send('<form method="post"><p>Check to <label>'
+            + '<input type="checkbox" name="remember"/> remember me</label> '
+            + '<input type="submit" value="Submit"/>.</p></form>');
+    }
 });
 
-app.listen(80);
-console.log('Application listening...');
+app.get('/forget', function(req, res){
+    res.clearCookie('remember');
+    res.redirect('back');
+});
+
+app.post('/', function(req, res){
+    console.log(req.body);
+    var minute = 60000;
+    if (req.body.remember) res.cookie('remember', 1, { maxAge: minute });
+    res.redirect('back');
+});
+
+if (!module.parent){
+    app.listen(80);
+    console.log('Express started on port 80');
+}
