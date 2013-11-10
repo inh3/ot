359a3380fee60fc2bc7efa0b8458b1f283757419
Ivan Hall <ivan.hall@gmail.com>
2013-11-09 19:33:30 -0600

    Adding stored proc to get tweets of followed users by user id.

---
+++ database/tweets/get_tweets_from_followed_users.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+DROP PROCEDURE GetTweetsFromFollowedUsers$$
+
+CREATE PROCEDURE GetTweetsFromFollowedUsers(IN userId INT)
+BEGIN
+  SELECT Tweets.user_name, Tweets.message, Tweets.timestamp
+  FROM Tweets
+  INNER JOIN Follow ON Follow.followed_user_id = Tweets.user_id
+  INNER JOIN Users ON Users.id = Follow.followed_user_id
+  WHERE Follow.user_id = userId;
+END$$
+DELIMITER ;
